2257cc9bee474ebda5e5361b23e77846f3623dd5
kiwon <kkw-11@naver.com>
2025-08-01 15:16:17 +0900

    first commit

---
+++ src/main/java/com/concurrency/concurrency_stock_order/ConcurrencyStockOrderApplication.java
@@ -0,0 +1,13 @@
+package com.concurrency.concurrency_stock_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyStockOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyStockOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concurrency/concurrency_stock_order/ConcurrencyStockOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurrency.concurrency_stock_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyStockOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
