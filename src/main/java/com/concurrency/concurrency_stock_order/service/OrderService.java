9f49850144c96453e78f45207bc6c4bad8e39aff
kiwon <kkw-11@naver.com>
2025-08-02 10:45:29 +0900

    feat:orderService

---
+++ src/main/java/com/concurrency/concurrency_stock_order/service/OrderService.java
@@ -0,0 +1,29 @@
+package com.concurrency.concurrency_stock_order.service;
+
+import com.concurrency.concurrency_stock_order.domain.Order;
+import com.concurrency.concurrency_stock_order.domain.ProductStock;
+import com.concurrency.concurrency_stock_order.domain.User;
+import com.concurrency.concurrency_stock_order.repository.OrderRepository;
+import com.concurrency.concurrency_stock_order.repository.ProductStockRepository;
+import com.concurrency.concurrency_stock_order.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final UserRepository userRepository;
+    private final ProductStockRepository productStockRepository;
+    private final OrderRepository orderRepository;
+
+    @Transactional
+    public void placeOrder(Long userId, Long productId, int quantity) {
+        User user = userRepository.findById(userId).orElseThrow();
+        ProductStock product = productStockRepository.findById(productId).orElseThrow();
+
+        product.decreaseStock(quantity); // 재고 차감
+        orderRepository.save(new Order(user, product, quantity));
+    }
+}
