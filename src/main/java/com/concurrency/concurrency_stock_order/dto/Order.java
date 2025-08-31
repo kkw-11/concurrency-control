ab8a99d3ea95d5e8afdac46991d5209efa2a4ad3
kiwon <kkw-11@naver.com>
2025-08-31 20:49:49 +0900

    feat:Order Entity 생성

---
+++ src/main/java/com/concurrency/concurrency_stock_order/dto/Order.java
@@ -0,0 +1,36 @@
+package com.concurrency.concurrency_stock_order.dto;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "orders") // order도 예약어라 보통 orders 사용
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime orderDate;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public void addOrderItem(OrderItem item) {
+        orderItems.add(item);
+        item.setOrder(this);
+    }
+}
+
