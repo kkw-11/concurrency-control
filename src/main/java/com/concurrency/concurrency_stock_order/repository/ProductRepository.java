b4a0257a4952fc1bf37e3883348655fa54e2d3af
kiwon <kkw-11@naver.com>
2025-09-01 09:55:03 +0900

    feat:Product 생성자 추가 및 클래스명 수정

--- src/main/java/com/concurrency/concurrency_stock_order/repository/ProductRepository.java
+++ src/main/java/com/concurrency/concurrency_stock_order/repository/ProductRepository.java
@@ -1,14 +1,14 @@
 package com.concurrency.concurrency_stock_order.repository;
 
-import com.concurrency.concurrency_stock_order.domain.ProductStock;
+import com.concurrency.concurrency_stock_order.domain.Product;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ProductStockRepository extends JpaRepository<ProductStock, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT p FROM ProductStock p WHERE p.id = :id")
-    ProductStock findByIdForUpdate(@Param("id") Long id);
+    @Query("SELECT p FROM Product p WHERE p.id = :id")
+    Product findByIdForUpdate(@Param("id") Long id);
 }
