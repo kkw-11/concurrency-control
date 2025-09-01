8bedb5b2e00c7df56e65dbc614d015f277642312
kiwon <kkw-11@naver.com>
2025-09-02 08:25:53 +0900

    feat:cursor 기반 상품 조회 메서드 생성, nativeQuery

--- src/main/java/com/concurrency/concurrency_stock_order/repository/ProductRepository.java
+++ src/main/java/com/concurrency/concurrency_stock_order/repository/ProductRepository.java
@@ -7,8 +7,13 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT p FROM Product p WHERE p.id = :id")
     Product findByIdForUpdate(@Param("id") Long id);
+
+    @Query(value = "select * from product p where p.id >= :id limit :pageSize", nativeQuery = true)
+    List<Product> findAllByCursor(@Param("id") Long id, @Param("pageSize") int pageSize);
 }
