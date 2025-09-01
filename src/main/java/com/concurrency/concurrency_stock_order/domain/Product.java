d8d2c418174385b468958d5224271788cafe4915
kiwon <kkw-11@naver.com>
2025-09-01 09:54:58 +0900

    feat:Product 생성자 추가 및 클래스명 수정

--- src/main/java/com/concurrency/concurrency_stock_order/domain/Product.java
+++ src/main/java/com/concurrency/concurrency_stock_order/domain/Product.java
@@ -7,20 +7,30 @@ import lombok.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ProductStock {
+public class Product {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
+    private Integer price;
+
     private Integer stock;
 
-    public ProductStock(String name, Integer stock) {
+    public Product(String name, Integer price, Integer stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    // 새로운 생성자 추가
+    public Product(String name, Integer stock) {
         this.name = name;
         this.stock = stock;
     }
 
+    // 재고 감소
     public void decreaseStock(int quantity) {
         if (this.stock < quantity) {
             throw new OutOfStockException();
