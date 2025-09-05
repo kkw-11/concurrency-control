18a432dfaad5cd018d5e25b8a0cd9b4ea7291c96
kiwon <kkw-11@naver.com>
2025-09-05 18:32:49 +0900

    test:테스트 코드 ActiveProfiles 설정

--- src/test/java/com/concurrency/concurrency_stock_order/ConcurrencyStockOrderApplicationTests.java
+++ src/test/java/com/concurrency/concurrency_stock_order/ConcurrencyStockOrderApplicationTests.java
@@ -2,8 +2,10 @@ package com.concurrency.concurrency_stock_order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ConcurrencyStockOrderApplicationTests {
 
 	@Test
